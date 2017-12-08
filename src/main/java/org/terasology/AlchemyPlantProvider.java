39dcd9780652f3b0c000be47e37f2067bd25c0bb
Thomas O'Keeffe <nitrogen1818@gmail.com>
2017-12-08 10:49:20 -0500

    first commit

---
+++ src/main/java/org/terasology/AlchemyPlantProvider.java
@@ -0,0 +1,87 @@
+package org.terasology;
+
+import org.terasology.entitySystem.Component;
+import org.terasology.math.geom.BaseVector2i;
+import org.terasology.rendering.nui.properties.Range;
+import org.terasology.utilities.procedural.Noise;
+import org.terasology.utilities.procedural.WhiteNoise;
+import org.terasology.world.generation.Border3D;
+import org.terasology.world.generation.ConfigurableFacetProvider;
+import org.terasology.world.generation.FacetProviderPlugin;
+import org.terasology.world.generation.GeneratingRegion;
+import org.terasology.world.generation.Produces;
+import org.terasology.world.generation.facets.SurfaceHeightFacet;
+import org.terasology.world.generator.plugin.RegisterPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RegisterPlugin
+@Produces(AlchemyPlantFacet.class)
+public class AlchemyPlantProvider implements FacetProviderPlugin, ConfigurableFacetProvider {
+
+    private AlchemyPlantConfiguration configuration = new AlchemyPlantConfiguration();
+
+    private Noise noise;
+
+    private Map<Float, Float> config_map = new HashMap<Float, Float>() {
+        private static final long serialVersionUID = 1L;
+        
+        {
+            put(0.0f, 1.0f);
+            put(25.0f, 0.998f);
+            put(50.0f, 0.992f);
+            put(75.0f, 0.99f);
+            put(100.0f, 0.97f);
+        }
+    };
+
+    @Override
+    public void setSeed(long seed) {
+        noise = new WhiteNoise(seed);
+    }
+
+    @Override
+    public void process(GeneratingRegion region) {
+
+        //Don't forget you sometimes have to extend the borders.
+        //extendBy(top, bottom, sides) is the method used for this.
+
+        Border3D border = region.getBorderForFacet(AlchemyPlantFacet.class);
+        AlchemyPlantFacet facet = new AlchemyPlantFacet(region.getRegion(), border);
+        SurfaceHeightFacet surfaceHeightFacet = region.getRegionFacet(SurfaceHeightFacet.class);
+
+        for (BaseVector2i position : surfaceHeightFacet.getWorldRegion().contents()) {
+            int surfaceHeight = (int) surfaceHeightFacet.getWorld(position);
+
+            if (facet.getWorldRegion().encompasses(position.getX(), surfaceHeight, position.getY())
+                && noise.noise(position.getX(), position.getY()) > config_map.get(configuration.plantRarity)) {
+                facet.setWorld(position.getX(), surfaceHeight, position.getY(), true);
+            }
+        }
+
+        region.setRegionFacet(AlchemyPlantFacet.class, facet);
+
+    }
+
+    @Override
+    public String getConfigurationName() {
+        return "Alchemy Plants";
+    }
+
+    @Override
+    public Component getConfiguration() {
+        return configuration;
+    }
+
+    @Override
+    public void setConfiguration(Component configuration) {
+        this.configuration = (AlchemyPlantConfiguration) configuration;
+    }
+
+    private static class AlchemyPlantConfiguration implements Component {
+        @Range(min = 0.0f, max = 100f, increment = 25f, precision = 1, description = "Plant Rarity")
+        private float plantRarity = 50f;
+    }
+
+}
---
+++ src/main/java/org/terasology/AlchemyPlantRasterizer.java
@@ -0,0 +1,69 @@
+package org.terasology;
+
+import org.terasology.math.ChunkMath;
+import org.terasology.math.geom.Vector3i;
+import org.terasology.registry.CoreRegistry;
+import org.terasology.world.block.Block;
+import org.terasology.world.block.BlockManager;
+import org.terasology.world.chunks.CoreChunk;
+import org.terasology.world.generation.Region;
+import org.terasology.world.generation.WorldRasterizerPlugin;
+import org.terasology.world.generator.plugin.RegisterPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+@RegisterPlugin
+public class AlchemyPlantRasterizer implements WorldRasterizerPlugin {
+    private Block antiPoison;
+    private Block healing;
+    private Block jumpSpeed;
+    private Block poison;
+    private Block rage;
+    private Block regen;
+    private Block swimSpeed;
+    private Block walkSpeed;
+
+    private Random rand;
+
+    private Map<Integer, Block> block_map = new HashMap<Integer, Block>() {
+        private static final long serialVersionUID = 1L;
+
+        {
+            put(0, antiPoison);
+            put(1, healing);
+            put(2, jumpSpeed);
+            put(3, poison);
+            put(4, rage);
+            put(5, regen);
+            put(6, swimSpeed);
+            put(7, walkSpeed);
+        }
+    };
+
+    @Override
+    public void initialize() {
+        antiPoison = CoreRegistry.get(BlockManager.class).getBlock("Alchemy:AntiPoisonHerb");
+        healing = CoreRegistry.get(BlockManager.class).getBlock("Alchemy:HealingHerb");
+        jumpSpeed = CoreRegistry.get(BlockManager.class).getBlock("Alchemy:JumpSpeedHerb");
+        poison = CoreRegistry.get(BlockManager.class).getBlock("Alchemy:Poison");
+        rage = CoreRegistry.get(BlockManager.class).getBlock("Alchemy:RageHerb");
+        regen = CoreRegistry.get(BlockManager.class).getBlock("Alchemy:RegenHerb");
+        swimSpeed = CoreRegistry.get(BlockManager.class).getBlock("Alchemy:SwimSpeedHerb");
+        walkSpeed = CoreRegistry.get(BlockManager.class).getBlock("Alchemy:WalkSpeedHerb");
+
+        rand = new Random();
+    }
+
+    @Override
+    public void generateChunk(CoreChunk chunk, Region chunkRegion) {
+        AlchemyPlantFacet alchemyPlantFacet = chunkRegion.getFacet(AlchemyPlantFacet.class);
+
+        for (Vector3i block : alchemyPlantFacet.getWorldRegion()) {
+            if (alchemyPlantFacet.getWorld(block)) {
+                chunk.setBlock(ChunkMath.calcBlockPos(block), block_map.get(rand.nextInt(8)));
+            }
+        }
+    }
+}
