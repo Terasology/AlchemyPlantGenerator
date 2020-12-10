cf1be00cdb413b110bb907ace3b1513f75bc3ecf
4Denthusiast <25589515+4Denthusiast@users.noreply.github.com>
2020-12-10 20:19:16 +0000

    Replace SurfaceHeightFacet with ElevationFacet and SurfacesFacet

--- src/main/java/org/terasology/AlchemyPlantProvider.java
+++ src/main/java/org/terasology/AlchemyPlantProvider.java
@@ -3,25 +3,25 @@
 package org.terasology;
 
 import org.terasology.entitySystem.Component;
-import org.terasology.math.geom.BaseVector2i;
 import org.terasology.nui.properties.Range;
 import org.terasology.utilities.procedural.Noise;
 import org.terasology.utilities.procedural.WhiteNoise;
 import org.terasology.world.generation.Border3D;
 import org.terasology.world.generation.ConfigurableFacetProvider;
 import org.terasology.world.generation.Facet;
+import org.terasology.world.generation.FacetBorder;
 import org.terasology.world.generation.FacetProviderPlugin;
 import org.terasology.world.generation.GeneratingRegion;
 import org.terasology.world.generation.Produces;
 import org.terasology.world.generation.Requires;
-import org.terasology.world.generation.facets.SurfaceHeightFacet;
+import org.terasology.world.generation.facets.SurfacesFacet;
 import org.terasology.world.generator.plugin.RegisterPlugin;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RegisterPlugin
-@Requires(@Facet(SurfaceHeightFacet.class))
+@Requires(@Facet(value = SurfacesFacet.class, border = @FacetBorder(bottom = 1)))
 @Produces(AlchemyPlantFacet.class)
 public class AlchemyPlantProvider implements FacetProviderPlugin, ConfigurableFacetProvider {
 
@@ -54,14 +54,16 @@ public class AlchemyPlantProvider implements FacetProviderPlugin, ConfigurableFa
 
         Border3D border = region.getBorderForFacet(AlchemyPlantFacet.class);
         AlchemyPlantFacet facet = new AlchemyPlantFacet(region.getRegion(), border);
-        SurfaceHeightFacet surfaceHeightFacet = region.getRegionFacet(SurfaceHeightFacet.class);
+        SurfacesFacet surfacesFacet = region.getRegionFacet(SurfacesFacet.class);
 
-        for (BaseVector2i position : surfaceHeightFacet.getWorldRegion().contents()) {
-            int surfaceHeight = (int) surfaceHeightFacet.getWorld(position);
-
-            if (facet.getWorldRegion().encompasses(position.getX(), surfaceHeight, position.getY())
-                && noise.noise(position.getX(), position.getY()) > configMap.get(configuration.plantRarity)) {
-                facet.setWorld(position.getX(), surfaceHeight, position.getY(), true);
+        for (int x = surfacesFacet.getWorldRegion().minX(); x <= surfacesFacet.getWorldRegion().maxX(); x++) {
+            for (int z = surfacesFacet.getWorldRegion().minZ(); z <= surfacesFacet.getWorldRegion().maxZ(); z++) {
+                for (int surfaceHeight : surfacesFacet.getWorldColumn(x, z)) {
+                    if (facet.getWorldRegion().encompasses(x, surfaceHeight + 1, z)
+                        && noise.noise(x, surfaceHeight, z) > configMap.get(configuration.plantRarity)) {
+                        facet.setWorld(x, surfaceHeight + 1, z, true);
+                    }
+                }
             }
         }
 
