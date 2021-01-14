1fa9299d1bfee4a0a1e65173a95e836867f2ef9f
Michael Pollind <polli104@mail.chapman.edu>
2021-01-14 12:01:59 -0800

    chore: cleanup code and migrate for Chunks (#6)
    * cleanup code and migrate for Chunks
    * simpliefy with a list

--- src/main/java/org/terasology/AlchemyPlantFacet.java
+++ src/main/java/org/terasology/AlchemyPlantFacet.java
@@ -1,3 +1,5 @@
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology;
 
 import org.terasology.world.block.BlockRegion;
--- src/main/java/org/terasology/AlchemyPlantRasterizer.java
+++ src/main/java/org/terasology/AlchemyPlantRasterizer.java
@@ -1,22 +1,29 @@
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology;
 
 import org.joml.Vector3i;
 import org.joml.Vector3ic;
-import org.terasology.math.ChunkMath;
-import org.terasology.registry.CoreRegistry;
+import org.terasology.registry.In;
 import org.terasology.world.block.Block;
 import org.terasology.world.block.BlockManager;
+import org.terasology.world.chunks.Chunks;
 import org.terasology.world.chunks.CoreChunk;
 import org.terasology.world.generation.Region;
 import org.terasology.world.generation.WorldRasterizerPlugin;
 import org.terasology.world.generator.plugin.RegisterPlugin;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
 @RegisterPlugin
 public class AlchemyPlantRasterizer implements WorldRasterizerPlugin {
+    @In
+    private BlockManager blockManager;
+
     private Block antiPoison;
     private Block healing;
     private Block jumpSpeed;
@@ -28,35 +35,30 @@ public class AlchemyPlantRasterizer implements WorldRasterizerPlugin {
 
     private Random rand;
 
-    private Map<Integer, Block> block_map;
+    private List<Block> herbList;
 
     @Override
     public void initialize() {
-        antiPoison = CoreRegistry.get(BlockManager.class).getBlock("Alchemy:AntiPoisonHerb");
-        healing = CoreRegistry.get(BlockManager.class).getBlock("Alchemy:HealingHerb");
-        jumpSpeed = CoreRegistry.get(BlockManager.class).getBlock("Alchemy:JumpSpeedHerb");
-        poison = CoreRegistry.get(BlockManager.class).getBlock("Alchemy:PoisonHerb");
-        rage = CoreRegistry.get(BlockManager.class).getBlock("Alchemy:RageHerb");
-        regen = CoreRegistry.get(BlockManager.class).getBlock("Alchemy:RegenHerb");
-        swimSpeed = CoreRegistry.get(BlockManager.class).getBlock("Alchemy:SwimSpeedHerb");
-        walkSpeed = CoreRegistry.get(BlockManager.class).getBlock("Alchemy:WalkSpeedHerb");
+        antiPoison = blockManager.getBlock("Alchemy:AntiPoisonHerb");
+        healing = blockManager.getBlock("Alchemy:HealingHerb");
+        jumpSpeed = blockManager.getBlock("Alchemy:JumpSpeedHerb");
+        poison = blockManager.getBlock("Alchemy:PoisonHerb");
+        rage = blockManager.getBlock("Alchemy:RageHerb");
+        regen = blockManager.getBlock("Alchemy:RegenHerb");
+        swimSpeed = blockManager.getBlock("Alchemy:SwimSpeedHerb");
+        walkSpeed = blockManager.getBlock("Alchemy:WalkSpeedHerb");
 
         rand = new Random();
 
-        block_map = new HashMap<Integer, Block>() {
-            private static final long serialVersionUID = 1L;
-
-            {
-                put(0, antiPoison);
-                put(1, healing);
-                put(2, jumpSpeed);
-                put(3, poison);
-                put(4, rage);
-                put(5, regen);
-                put(6, swimSpeed);
-                put(7, walkSpeed);
-            }
-        };
+        herbList = new ArrayList<>();
+        herbList.add(antiPoison);
+        herbList.add(healing);
+        herbList.add(jumpSpeed);
+        herbList.add(poison);
+        herbList.add(rage);
+        herbList.add(regen);
+        herbList.add(swimSpeed);
+        herbList.add(walkSpeed);
     }
 
     @Override
@@ -65,7 +67,7 @@ public class AlchemyPlantRasterizer implements WorldRasterizerPlugin {
 
         for (Vector3ic block : alchemyPlantFacet.getWorldRegion()) {
             if (alchemyPlantFacet.getWorld(block)) {
-                chunk.setBlock(ChunkMath.calcRelativeBlockPos(block, new Vector3i()), block_map.get(rand.nextInt(8)));
+                chunk.setBlock(Chunks.toRelative(block, new Vector3i()), herbList.get(rand.nextInt(herbList.size())));
             }
         }
     }
